fbc58a0d517cff7a9fc5d9dde6534d0f15769675
Kanyango <kariukikanyango@gmail.com>
2017-11-03 13:37:37 +0300

    Update app.js

--- app.js
+++ app.js
@@ -43,7 +43,7 @@ app.use(function(req, res, next) {
     res.header('Access-Control-Allow-Origin', "*");  
     res.header('Access-Control-Expose-Headers', "Authorization");
     res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
-    res.header('Access-Control-Allow-Headers', 'Content-Type');
+    res.header('Access-Control-Allow-Headers', 'Content-Type','Authorization');
     next();
 });
 

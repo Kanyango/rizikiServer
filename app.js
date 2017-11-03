174cbedb37e929187ffa4e6d1c899d9db7ccd053
Kanyango <kariukikanyango@gmail.com>
2017-11-03 13:31:56 +0300

    Update app.js

--- app.js
+++ app.js
@@ -40,7 +40,8 @@ app.use(passport.initialize());
 app.use(passport.session());
 
 app.use(function(req, res, next) {
-    res.header('Access-Control-Allow-Origin', "*");
+    res.header('Access-Control-Allow-Origin', "*");  
+    res.header('Access-Control-Expose-Headers', "Authorization");
     res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
     res.header('Access-Control-Allow-Headers', 'Content-Type');
     next();

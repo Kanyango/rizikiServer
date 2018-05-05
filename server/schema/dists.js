7441eae6cc63c68dd24143b58339fc31bf0afee2
Kanyango <kariukikanyango@gmail.com>
2018-05-05 13:14:42 +0300

    Update dists.js

--- server/schema/dists.js
+++ server/schema/dists.js
@@ -9,10 +9,11 @@ module.exports = function(app , mongoose)
 	var distSchema = new mongoose.Schema({
 
       dateCreated: {type: Date, default: Date.now()},
-      biz_name : {type: String},
-      biz_mail  : {type: String},
-      biz_phone  : {type: String},
-      biz_password  : {type: String}
+      biz_name      : {type: String},
+      biz_mail      : {type: String},
+      biz_phone     : {type: String},
+      biz_password  : {type: String},
+      coverage      : []
  
 	});
 	app.db.model('Distributor', distSchema);

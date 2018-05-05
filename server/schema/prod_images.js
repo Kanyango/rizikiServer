4356febf7b09a5f87fa4e33c34d416dd1c7676ce
Kanyango <kariukikanyango@gmail.com>
2018-05-05 12:20:55 +0300

    Update prod_images.js

--- server/schema/prod_images.js
+++ server/schema/prod_images.js
@@ -1,1 +1,16 @@
 
+
+'use strict';
+
+module.exports = function(app , mongoose){
+
+	var prodImageSchema = new mongoose.Schema({
+
+       //user       : {type: mongoose.Schema.Types.ObjectId, ref: 'User'},
+		dateCreated : {type : Date,default: Date.now()},
+		img_title 			: {type: String},
+    img_url        	: {type: String},
+		
+	});
+	app.db.model('ProdImage' , prodImageSchema);
+};

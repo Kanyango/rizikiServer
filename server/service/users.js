969d97060eccc9e6f9e377eb98a509663a98e8e5
Kanyango <kariukikanyango@gmail.com>
2018-05-12 19:49:46 +0300

    Update users.js

--- server/service/users.js
+++ server/service/users.js
@@ -141,7 +141,7 @@ var user = {
             });
         }
 	},*/
-	settings  : function(req , res , next)
+	readProfile  : function(req , res , next)
 	{
 		req.app.db.models.User.findById(req.payload._id, 
 			function(err , info){

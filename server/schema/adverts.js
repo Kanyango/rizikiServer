8f83720a3fd7cc8332f29a23d0c77650a75802fe
Kanyango <kariukikanyango@gmail.com>
2018-05-24 17:59:41 +0300

    Update adverts.js

--- server/schema/adverts.js
+++ server/schema/adverts.js
@@ -1,1 +1,22 @@
 
+'use strict';
+
+var mongoose = require('mongoose');
+
+module.exports = function(app , mongoose)
+{
+	var advertsSchema = new mongoose.Schema({
+
+	dateCreated: {type: Date, default: Date.now()},
+	title      : {type: String},
+  status     : {type: String},
+  owner      : {type: String},
+  cost       : {type: String},
+  position   : {type: String},
+  photo      : {type: String},
+  start_date : {type: String},
+  end_date   : {type: String}
+ 
+	});
+	app.db.model('Adverts', advertsSchema);
+};

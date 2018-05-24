88e724bd9f4c1a54f8d8351ffe2d869e0bc1d1eb
Kanyango <kariukikanyango@gmail.com>
2018-05-24 18:00:04 +0300

    Update models.js

--- models.js
+++ models.js
@@ -8,4 +8,5 @@ module.exports = function(app , mongoose)
 	require('./server/schema/products')(app , mongoose);
 	require('./server/schema/dists')(app , mongoose);
 	require('./server/schema/prod_images')(app , mongoose);
+	require('./server/schema/adverts')(app , mongoose);
 };

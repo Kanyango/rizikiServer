91134bbc3b1535bc581a978398bea164d7372414
Kanyango <kariukikanyango@gmail.com>
2018-05-24 18:54:57 +0300

    Update adverts.js

--- server/service/adverts.js
+++ server/service/adverts.js
@@ -109,9 +109,9 @@ var adverts = {
 
 		var storage = multer.diskStorage({
   // destination
-		  destination: function (req, file, cb) {
+		  /*destination: function (req, file, cb) {
 		    cb(null, './uploads/')
-		  },
+		  },*/
 		  filename: function (req, file, cb) {
 		    cb(null, file.originalname);
 		  }

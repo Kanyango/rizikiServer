37d17972a7780441130e02ff6d247c9699d0f2e2
Kanyango <kariukikanyango@gmail.com>
2018-05-25 08:01:29 +0300

    Update categories.js

--- server/service/categories.js
+++ server/service/categories.js
@@ -1,1 +1,100 @@
 
+'use strict';
+
+var mongoose = require('mongoose');
+var multer = require('multer');
+var formidable = require('formidable');
+//var upload = multer({dest: DIR}).single('photo');
+//var DIR = './uploads/';
+var apikey   = 'b08b2c77192e5dc068f327209015659596c3eb85cda37524729622dd0968d53e';
+var cloudinary = require('cloudinary');
+
+var cats = {
+
+	create : function(req , res , next)
+	{
+		var fieldsToSet =
+		{
+			cat_title      : req.body.cat_title,
+		};
+
+		req.app.db.models.Categories.create(fieldsToSet,
+			function(err ,  docs){
+
+				if(err)
+				{
+					return next(err);
+				}
+				res.status(200).json(docs);
+			});
+	},
+	
+	read : function(req , res , next)
+	{
+		//var id = mongoose.Types.ObjectId(req.payload._id);
+		
+		req.app.db.models.Categories.find({},
+		    function(err , docs)
+			{
+				if(err)
+				{
+					return next(err);
+				}
+				res.status(200).json(docs);
+			});
+	},
+
+	upload: function(req, res, next)
+	{
+		var id = mongoose.Types.ObjectId(req.params.id);
+
+		var storage = multer.diskStorage({
+  // destination
+		  /*destination: function (req, file, cb) {
+		    cb(null, './uploads/')
+		  },*/
+		  filename: function (req, file, cb) {
+		    cb(null, file.originalname);
+		  }
+		});
+
+	var upload = multer({ //multer settings
+                    storage: storage
+                }).single('photo');
+
+
+
+							            //var fieldsToSet = { img : {data : fs.readFileSync(req.file.path, "base64"), contentType : 'img/png' } };
+
+
+	upload(req,res,function(err){
+						            if(err){
+						                 //res.json({error_code:1,err_desc:err});
+						                return next(err);
+						            }
+
+												 var pathy = req.file.path;
+												 cloudinary.config({
+												 cloud_name: 'dxomvhu0p',
+												 api_key: '811296612498678',
+												 api_secret: 'j8BV1pcR-Jagxi63jCJSAMrImVM'
+											 });
+											 cloudinary.uploader.upload(pathy,
+											 function(result) {
+												  var fieldsToSet = { cat_img : result.secure_url };
+													var options = { new: true };
+			req.app.db.models.Categories.findByIdAndUpdate(id, fieldsToSet, options, function(err , docs){
+													if(err)
+											    	{
+											    		return next(err);
+											    	}
+														res.status(200).json(docs);
+											 });
+
+						        });
+									});
+
+	}
+
+}
+module.exports = cats;

171ad7be92b0daa187b479d0de373c8b1e80cbe5
Kanyango <kariukikanyango@gmail.com>
2018-05-25 07:37:05 +0300

    Update adverts.js

--- server/schema/adverts.js
+++ server/schema/adverts.js
@@ -9,13 +9,16 @@ module.exports = function(app , mongoose)
 
 	dateCreated: {type: Date, default: Date.now()},
 	title      : {type: String},
-  status     : {type: String},
-  owner      : {type: String},
-  cost       : {type: String},
-  position   : {type: String},
-  photo      : {type: String},
-  start_date : {type: String},
-  end_date   : {type: String}
+	  status     : {type: String},
+	  owner      : {type: String},
+	  cost       : {type: String},
+	  position   : {type: String},
+	  photo      : {type: String},
+	  start_date : {type: String},
+	  end_date   : {type: String},
+	  brand      : {type: String},
+          clicks     : {type: String},
+	  views      : {type: String}
  
 	});
 	app.db.model('Adverts', advertsSchema);
